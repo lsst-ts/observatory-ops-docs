2c60cee022e0ff7946884dc3f680ee952e74710f
Paulo Lago <plago@lsst.org>
2025-03-10 16:49:54 -0300

    Migrate AuxTel PDU Outlets Mapping to obs-ops.lsst.io

--- conf.py
+++ conf.py
@@ -4,3 +4,12 @@ from documenteer.conf.guide import *
 # directories to ignore when looking for source files.
 
 exclude_patterns += ["project/Templates/Template-Folder"]
+linkcheck_ignore = [
+    r'http://aux-pdu-tcs.cp.lsst.org',
+    r'http://pdu1-aux.cp.lsst.org', 
+    r'http://pdu2-aux.cp.lsst.org',
+    r'http://aux-pdu-spectrograph.cp.lsst.org', 
+    r'http://aux-pdu-dome.cp.lsst.org',
+    r'http://auxtel-illpdu.cp.lsst.org/.*', 
+    r'http://aux-pdu-latiss.cp.lsst.org/.*',
+]
